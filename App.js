976dc524f1de57f0bcd08f4518a9f418206ba632
paingzin11 <paingzinphyo040@gmail.com>
2023-01-06 12:50:02 +0630

    async

--- App.js
+++ App.js
@@ -1,11 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  const [name, setName] = useState();
+
+  const save = async () => {
+    try {
+      await AsyncStorage.setItem("MyName", name);
+    } catch (err) {
+        alert(err);
+    }
+
+    [/*let user = {
+        name: "DesignIntoCode",
+        location: "US",
+    };
+  await AsyncStorage.setItem("MyName", JSON.stringify(user));*/]
+    
+  };
+
+  const load = async () => {
+    try {
+      let name = await AsyncStorage.getItem("MyName")
+
+    if (name !== null) {
+        setName(name);
+      }
+
+      [/*  let jsonValue = await AsyncStorage.getItem("MyName")
+
+      if (jsonValue != null) {
+        setName(JSON.parse(jsonValue))
+      }
+    */]  
+
+
+    } catch (err) {
+        alert(err);
+    }
+  };
+
+  const remove = async () => {
+    try {
+      await AsyncStorage.removeItem("MyName")
+    } catch (err) {
+        alert(err)
+    } finally {
+      setName("");
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./assets/welcome.png")}
+        style={{width: "100%", height: 200, margin: 64}}
+        resizeMode="contain"
+      />
+
+      <Text style={{height: 30}}>{name}</Text>
+      <Text style={styles.name}>What's your name?</Text>
+
+      <TextInput style={styles.input} onChangeText={(text) => setName(text)} />
+
+      <TouchableOpacity style={styles.button} onPress={() => save()}>
+        <Text style={{color: "white"}}>Save my name!</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => remove()} >
+        <Text style={{color: "white"}}>Remove my name!</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -15,6 +85,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  name: {
+    fontSize: 24,
+    fontWeight: '300',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#575dd9",
+    alignSelf: 'stretch',
+    margin: 32,
+    height: 64,
+    borderRadius: 6,
+    paddingHorizontal: 16,
+    fontSize: 24,
+    fontWeight: '300',
+  },
+  button: {
+    backgroundColor: '#575dd9',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    marginTop: 32,
+    marginHorizontal: 32,
+    borderRadius: 6,
+  },
+
 });
